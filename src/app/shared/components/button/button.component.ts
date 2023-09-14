c57a251d14c4bba02dbcb9e423a091572aa011f7
Pedro Henrique <opedro3g@gmail.com>
2023-09-14 11:57:42 -0300

    feat: input component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -8,5 +8,21 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() variant: 'outlined' | 'filled' = 'outlined';
   @Input() radius: 'rounded' | 'square' = 'rounded';
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+  @Input() horizontalPadding: 'small' | 'medium' | 'large' = 'medium';
   @Input() value: string = '';
+  @Input() disabled: boolean = false;
+
+  private getHorizontalPadding() {
+    return `padding-${this.horizontalPadding}`;
+  }
+
+  getClasses() {
+    return [
+      this.variant,
+      this.radius,
+      this.size,
+      this.getHorizontalPadding(),
+    ].join(' ');
+  }
 }
